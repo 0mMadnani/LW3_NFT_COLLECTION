1e5835dbc697464479f5f54a0c6ba151b1068360
Om Madnani <77526602+0mMadnani@users.noreply.github.com>
2022-08-08 14:03:30 +0530

    Initial Commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0xF794eA63352F4973f60C357E2c1931842B5B4f70";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
